be0a9bc96533cfb5353507b4c9d75fe8ba35ec1d
darshan019 <darshandalal3@outlook.com>
2020-07-21 10:56:58 +0530

    upgrade to django version to 3.0.8

--- jobs/models.py
+++ jobs/models.py
@@ -1,5 +1,10 @@
 from django.db import models
 
 class Job(models.Model):
-    image = models.ImageField(upload_to='images/')
-    summary = models.CharField(max_length=200)
+    title = models.CharField(max_length=100)
+    description = models.CharField(max_length=250)
+    image = models.ImageField(upload_to='portfolio/images/')
+    url = models.URLField(blank=True)
+
+    def __str__(self):
+        return self.title
--- portfolio/urls.py
+++ portfolio/urls.py
@@ -1,7 +1,7 @@
-"""portfolio URL Configuration
+"""personal_portfolio URL Configuration
 
 The `urlpatterns` list routes URLs to views. For more information please see:
-    https://docs.djangoproject.com/en/2.2/topics/http/urls/
+    https://docs.djangoproject.com/en/3.0/topics/http/urls/
 Examples:
 Function views
     1. Add an import:  from my_app import views
@@ -15,12 +15,14 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
-from django.conf import settings
 from django.conf.urls.static import static
-import jobs.views
+from django.conf import settings
+from jobs import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', jobs.views.home, name='home'),
+    path('', views.home, name='home'),
     path('blog/', include('blog.urls')),
-] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+]
+
+urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
